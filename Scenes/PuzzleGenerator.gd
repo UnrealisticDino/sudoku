793b54e8d7242dd5bbc731c22a82d3a72bdde29d
UnrealisticDino <jdzubieta@gmail.com>
2023-08-22 15:41:43 -0500

    rework pt 5

--- Scenes/PuzzleGenerator.gd
+++ Scenes/PuzzleGenerator.gd
@@ -1,7 +1,16 @@
 #PuzzleGenerator
 extends Node
 
-func generate_full_grid():
+var difficulty = "Easy"  # Default difficulty
+
+# This function generates a puzzle based on the provided difficulty.
+func generate_puzzle(difficulty):
+	var puzzle = _create_base_puzzle()  # Create a base solved puzzle
+	puzzle = _remove_numbers_based_on_difficulty(puzzle, difficulty)
+	return puzzle
+
+# Create a base solved puzzle
+func _create_base_puzzle():
 	var grid = []
 	for row in range(9):
 		var row_data = []
@@ -10,21 +19,22 @@ func generate_full_grid():
 		grid.append(row_data)
 	# Fill the diagonal 3x3 subgrids
 	for i in range(0, 9, 3):
-		fill_subgrid(grid, i, i)
+		_fill_subgrid(grid, i, i)
 	# Fill the remaining cells
-	solve_grid(grid)
-
+	_solve_grid(grid)
 	return grid
 
-func fill_subgrid(grid, row, col):
+# Fill a 3x3 subgrid
+func _fill_subgrid(grid, row, col):
 	var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 	numbers.shuffle()
 	for i in range(3):
 		for j in range(3):
 			grid[row + i][col + j] = numbers.pop_front()
 
-func solve_grid(grid):
-	var empty_cell = find_empty_cell(grid)
+# Solve the grid
+func _solve_grid(grid):
+	var empty_cell = _find_empty_cell(grid)
 	if empty_cell == null:
 		return true # Puzzle is solved
 
@@ -32,32 +42,40 @@ func solve_grid(grid):
 	var col = empty_cell[1]
 
 	for num in range(1, 10):
-		if is_safe_to_place_number(grid, row, col, num):
+		if _is_safe_to_place_number(grid, row, col, num):
 			grid[row][col] = num
-			if solve_grid(grid):
-				return true # Continue if the solution is found
-			grid[row][col] = 0 # Backtrack if no solution is found
+			if _solve_grid(grid):
+				return true
+			grid[row][col] = 0
 
-	return false # Trigger backtracking
+	return false
 
-func find_empty_cell(grid):
+# Find an empty cell in the grid
+func _find_empty_cell(grid):
 	for row in range(9):
 		for col in range(9):
 			if grid[row][col] == 0:
 				return [row, col]
 	return null
 
-func is_safe_to_place_number(grid, row, col, num):
-	# Check row, column, and 3x3 subgrid
+# Check if it's safe to place a number in the specified cell
+func _is_safe_to_place_number(grid, row, col, num):
 	for x in range(9):
 		if grid[row][x] == num or grid[x][col] == num or grid[row - row % 3 + int(x / 3)][col - col % 3 + x % 3] == num:
 			return false
 	return true
 
-func generate_puzzle(grid, clues):
-	var puzzle = []
-	for row in grid:
-		puzzle.append(row.duplicate())
+# Remove numbers from the puzzle based on difficulty
+func _remove_numbers_based_on_difficulty(puzzle, difficulty):
+	var clues = 0
+	match difficulty:
+		"Easy":
+			clues = 35
+		"Medium":
+			clues = 25
+		"Hard":
+			clues = 20
+
 	var cells_to_remove = 81 - clues
 	while cells_to_remove > 0:
 		var row = randi() % 9
@@ -65,4 +83,5 @@ func generate_puzzle(grid, clues):
 		if puzzle[row][col] != 0:
 			puzzle[row][col] = 0
 			cells_to_remove -= 1
+
 	return puzzle
