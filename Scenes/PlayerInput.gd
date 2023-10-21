e310d2c09761ed3941ff005662c42d056c89bf91
UnrealisticDino <jdzubieta@gmail.com>
2023-10-21 05:53:21 -0500

    ui update

--- Scenes/PlayerInput.gd
+++ Scenes/PlayerInput.gd
@@ -7,30 +7,48 @@ var hint_box
 var hint_box_outline
 var hint_label
 var is_hint_box_pressed = false
+var grid_cells = []
+var scaled_cell_size = 220
 
 func _ready():
 	add_child(SudokuSolver)
+	
+	# Initialize 3x3 grid with numbers 1-9
+	for row in range(3):
+		for col in range(3):
+			var cell = Sprite.new()
+			cell.set_script(preload("res://Scenes/ImageDisplay.gd"))
+			cell.call("set_number", row * 3 + col + 1, "player")
+			cell.position = Vector2(col * scaled_cell_size + scaled_cell_size/2, row * scaled_cell_size + scaled_cell_size/2)
+			add_child(cell)
+			grid_cells.append(cell)
+	
 	# Create the outline for the hint box
 	hint_box_outline = ColorRect.new()
-	hint_box_outline.rect_min_size = Vector2(104, 44)  # Slightly larger than the hint_box
-	hint_box_outline.color = Global.grid_lines_color  # Set the outline color
+	hint_box_outline.rect_position = Vector2(scaled_cell_size * 3 + 20, scaled_cell_size)
+	hint_box_outline.rect_min_size = Vector2(204, 84)
+	hint_box_outline.color = Global.grid_lines_color
 	add_child(hint_box_outline)
 
 	# Create the hint box
 	hint_box = ColorRect.new()
-	hint_box.rect_min_size = Vector2(100, 40)  # Set the size of the box
-	hint_box.color = Color(1, 1, 1, 1)  # Set the color to white
+	hint_box.rect_min_size = Vector2(200, 80)
+	hint_box.rect_position = Vector2(4, 4)
 	hint_box.rect_position = Vector2(2, 2)  # Position it within the outline
 	hint_box_outline.add_child(hint_box)  # Add hint_box as a child of the outline
-	
+
 	# Create the label
 	hint_label = Label.new()
 	hint_label.text = "Hint"
-	hint_label.rect_min_size = Vector2(100, 40)
+	hint_label.rect_min_size = Vector2(200, 80)
 	hint_label.align = Label.ALIGN_CENTER  # Center align horizontally
 	hint_label.valign = Label.VALIGN_CENTER  # Center align vertically
 	hint_label.add_color_override("font_color", Global.game_placed_digit_color)  # Set the text color to green
 	hint_box.add_child(hint_label)
+	hint_label.rect_scale = Vector2(2, 2)
+	hint_label.rect_position = Vector2((hint_box.rect_min_size.x - hint_label.rect_min_size.x * hint_label.rect_scale.x) / 2, (hint_box.rect_min_size.y - hint_label.rect_min_size.y * hint_label.rect_scale.y) / 2)
+	
+	_draw()
 
 func _input(event):
 	if event is InputEventMouseButton or event is InputEventMouseMotion:
@@ -62,3 +80,25 @@ func _swap_colors():
 func _on_HintBox_pressed():
 	SudokuSolver.solve(Global.puzzle, Global.filled_sudoku, "PlayerInput")
 	yield(get_tree().create_timer(1.0), "timeout")
+
+func _draw():
+	# Draw grid lines
+	var grid_size = 3
+	var start_x = 0
+	var start_y = 0
+	var grid_color = Global.grid_lines_color
+
+	for i in range(grid_size + 1):
+		var start_point = Vector2(start_x + i * scaled_cell_size, start_y)
+		var end_point = Vector2(start_x + i * scaled_cell_size, start_y + grid_size * scaled_cell_size)
+		draw_line(start_point, end_point, grid_color, 2)
+
+		if i % 3 == 0:
+			draw_line(start_point, end_point, grid_color, 4)
+
+		start_point = Vector2(start_x, start_y + i * scaled_cell_size)
+		end_point = Vector2(start_x + grid_size * scaled_cell_size, start_y + i * scaled_cell_size)
+		draw_line(start_point, end_point, grid_color, 2)
+
+		if i % 3 == 0:
+			draw_line(start_point, end_point, grid_color, 4)
