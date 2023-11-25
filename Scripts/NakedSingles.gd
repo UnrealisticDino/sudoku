26a1910ea498147a42fb3e891a7f963c99ae3417
UnrealisticDino <jdzubieta@gmail.com>
2023-11-25 06:20:14 -0600

    solverupdate

---
+++ Scripts/NakedSingles.gd
@@ -0,0 +1,49 @@
+# NakedSingles.gd
+extends Node
+
+# Function to solve naked singles in a Sudoku grid
+func solve_naked_singles(grid: Array) -> Array:
+	var updated = false
+	var possibilities = Array()
+	
+	# Initialize an array to store possible numbers for each cell
+	for i in range(9):
+		possibilities.append([])
+		for j in range(9):
+			possibilities[i].append([])
+
+	# Iterate over each cell in the grid
+	for row in range(9):
+		for col in range(9):
+			# Skip if the cell is already filled
+			if grid[row][col] != 0:
+				continue
+
+			# Determine possible numbers for the current cell
+			for num in range(1, 10):
+				if is_num_possible(grid, row, col, num):
+					possibilities[row][col].append(num)
+
+			# If there's only one possibility, it's a naked single
+			if possibilities[row][col].size() == 1:
+				grid[row][col] = possibilities[row][col][0]
+				updated = true
+
+	return [grid, updated]
+
+# Helper function to check if a number can be placed in a specific cell
+func is_num_possible(grid: Array, row: int, col: int, num: int) -> bool:
+	# Check row and column
+	for i in range(9):
+		if grid[row][i] == num or grid[i][col] == num:
+			return false
+
+	# Check 3x3 subgrid
+	var start_row = (row / 3) * 3
+	var start_col = (col / 3) * 3
+	for i in range(start_row, start_row + 3):
+		for j in range(start_col, start_col + 3):
+			if grid[i][j] == num:
+				return false
+
+	return true
---
+++ Scripts/NakedTriples.gd
@@ -0,0 +1,116 @@
+#NakedTriples.gd
+extends Node
+
+# Main script to solve Naked Triples in a Sudoku grid
+class_name NakedTriplesSolver
+
+var grid : Array
+
+func _init(puzzle_grid : Array):
+	grid = puzzle_grid
+
+func solve():
+	var updated = false
+
+	# Check each row, column, and box for Naked Triples
+	for i in range(9):
+		updated = check_naked_triples_unit(i, true) or updated  # Check rows
+		updated = check_naked_triples_unit(i, false) or updated  # Check columns
+		updated = check_naked_triples_box(i) or updated  # Check boxes
+
+	return [grid, updated]
+
+# Helper function to check and resolve Naked Triples in a row or column
+func check_naked_triples_unit(index: int, is_row: bool) -> bool:
+	var updated = false
+	var cell_groups = get_cell_groups(grid, index, is_row)  # You need to implement this
+
+	for i in range(cell_groups.size()):
+		for j in range(i + 1, cell_groups.size()):
+			for k in range(j + 1, cell_groups.size()):
+				var combined = cell_groups[i] + cell_groups[j] + cell_groups[k]
+				combined = combined.deduplicate()  # Removes duplicates from the array
+
+				if combined.size() == 3:
+					# Found a naked triple, now eliminate these numbers from other cells
+					updated = eliminate_numbers(grid, index, is_row, combined, [i, j, k]) or updated  # Implement this
+
+	return updated
+
+# Helper function to check and resolve Naked Triples in a 3x3 box
+func check_naked_triples_box(box: int) -> bool:
+	var updated = false
+	var cell_groups = get_box_cell_groups(grid, box)
+
+	# Iterate through all combinations of three cells in the cell_groups
+	for i in range(cell_groups.size()):
+		for j in range(i + 1, cell_groups.size()):
+			for k in range(j + 1, cell_groups.size()):
+				var combined = cell_groups[i] + cell_groups[j] + cell_groups[k]
+				combined = combined.deduplicate()  # Removes duplicates from the array
+
+				# Check if the combined group forms a naked triple
+				if combined.size() == 3:
+					# Found a naked triple, now eliminate these numbers from other cells in the box
+					updated = eliminate_numbers_from_box(grid, box, combined, [i, j, k]) or updated
+
+	return updated
+
+func eliminate_numbers_from_box(grid: Array, box: int, numbers: Array, exceptions: Array) -> bool:
+	var updated = false
+	var start_row = (box / 3) * 3
+	var start_col = (box % 3) * 3
+
+	for row in range(start_row, start_row + 3):
+		for col in range(start_col, start_col + 3):
+			if not ([row, col] in exceptions):
+				var cell = grid[row][col]
+				if cell is Array:
+					for number in numbers:
+						if number in cell:
+							cell.erase(number)
+							updated = true
+
+	return updated
+
+func get_box_cell_groups(grid: Array, box: int) -> Array:
+	var cell_groups = []
+	var start_row = (box / 3) * 3
+	var start_col = (box % 3) * 3
+
+	for row in range(start_row, start_row + 3):
+		for col in range(start_col, start_col + 3):
+			var cell = grid[row][col]
+			if cell is Array:
+				cell_groups.append(cell)
+	return cell_groups
+
+func get_cell_groups(grid: Array, index: int, is_row: bool) -> Array:
+	var cell_groups = []
+	for i in range(9):
+		var cell
+		if is_row:
+			cell = grid[index][i]
+		else:
+			cell = grid[i][index]
+		
+		if cell is Array:  # Assuming a cell with multiple candidates is an Array
+			cell_groups.append(cell)
+	return cell_groups
+
+func eliminate_numbers(grid: Array, index: int, is_row: bool, numbers: Array, exceptions: Array) -> bool:
+	var updated = false
+	for i in range(9):
+		if not (i in exceptions):
+			var cell
+			if is_row:
+				cell = grid[index][i]
+			else:
+				cell = grid[i][index]
+
+			if cell is Array:
+				for number in numbers:
+					if number in cell:
+						cell.erase(number)
+						updated = true
+	return updated
